5512ae63e4a7d5d034602036a547cc2df48754a3
Sigma <trashalphonse@gmail.com>
2021-05-18 22:36:42 +0300

    fix BEM notation and add different text into hero section

--- app.js
+++ app.js
@@ -19,6 +19,24 @@ function moveSlides(n) {
 
 function showSlides(n) {
     const slides = document.querySelectorAll(".carousel__slide");
+
+    // HERO CONTENT
+
+    const heroContent = [
+        {
+            title: "Discover innovative ways to decorate",
+            text: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love."
+        },
+        {
+            title: "We are available all across the globe",
+            text: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today."
+        },
+        {
+            title: "Manufactured with the best materials",
+            text: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
+        }
+    ];
+
     if (n > slides.length) {
         slideIndex = 1;
     }
@@ -31,6 +49,8 @@ function showSlides(n) {
     }
 
     slides[slideIndex-1].classList.add("visible");
+    document.getElementById("heroTitle").innerHTML = heroContent[slideIndex-1].title;
+    document.getElementById("heroText").innerHTML = heroContent[slideIndex-1].text;
 
 }
 
